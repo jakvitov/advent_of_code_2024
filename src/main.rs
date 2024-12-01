7c52c001e2a7779c129b698fb2cab01e2fddbcbe
Jakub Vítovec <jakvitov@gmail.com>
2024-12-01 10:50:52 +0100

    Day1 first puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use std::fs;
 
-fn get_input() -> [Vec<i32>; 2] {
-       return [vec![], vec![]] 
-}
-
 fn main() {
-    let input: Result<String, std::io::Error> = fs::read_to_string("day1/day1_input");
-    let str:String = input.unwrap();
-    println!("{str}");
+    let read_file: Result<String, std::io::Error> = fs::read_to_string("src/day1/day1_input");
+    let input:String = read_file.unwrap();
+
+    let rows: Vec<&str> = input.split("\n").collect();
+    
+    let mut first_list: Vec<i32> = vec![];
+    let mut second_list: Vec<i32> = vec![];
+
+    for row in rows {
+        let items: Vec<&str> = row.split_whitespace().collect();
+        first_list.push(items[0].parse::<i32>().unwrap());
+        second_list.push(items[1].parse::<i32>().unwrap());
+    };
+
+    first_list.sort();
+    second_list.sort();
+   
+    let mut sum = 0;
+
+    for i in 0..first_list.len() {
+        sum = sum + (first_list[i] - second_list[i]).abs()
+    }
+
+    println!("{}", sum);
+
 }

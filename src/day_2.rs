45ac86f172962a279d7d706fb34449d70197a8c4
Jakub Vítovec <jakvitov@gmail.com>
2024-12-02 13:51:58 +0100

    Day 2 task 1 refactoring

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,13 +1,31 @@
 
-pub fn get_input() -> Vec<Vec<i32>> {
-    let file_str: String = include_str!("/src/inputs/day2_input");
+fn get_input() -> Vec<Vec<i32>> {
+    let file_str: String = String::from(include_str!("inputs/day2_input"));
 
-    let lines = file_str.split("\n").collect();
+    let lines: Vec<String> = file_str.split("\n").map(|x| String::from(x)).collect();
     
-    let res:  Vec<Vec<i32>> = Vec::new();
-
-    line.map(|x| {
-        x.split_whitespace().collect()
-    });
-    
+    return lines.into_iter().map(|line| line.split_whitespace().map(|num| num.parse::<i32>().unwrap()).collect()).collect()
 }
+
+fn is_level_valid(level: &Vec<i32>) -> bool {
+    if !(level.is_sorted() || level.is_sorted_by(|x, y| x > y)) {
+        //println!("Level not sorted: {:?}", level);
+        return false;
+    }
+    let mut diffs_valid = true;
+    for i in 1..level.len() {
+        let diff = (level[i-1] - level[i]).abs();
+        if diff < 1 || diff > 3 {
+            diffs_valid = false;
+            //println!("Level not diff: {:?}", level);
+            break;
+        } 
+    }
+    return diffs_valid
+}
+
+pub fn first() -> usize {
+    let levels  = get_input();
+    return levels.into_iter().filter(|level| is_level_valid(&level)).count();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
 mod day_1;
-
-fn is_level_valid(level: &Vec<i32>) -> bool {
-    if !(level.is_sorted() || level.is_sorted_by(|x, y| x > y)) {
-        //println!("Level not sorted: {:?}", level);
-        return false;
-    }
-    let mut diffs_valid = true;
-    for i in 1..level.len() {
-        let diff = (level[i-1] - level[i]).abs();
-        if diff < 1 || diff > 3 {
-            diffs_valid = false;
-            //println!("Level not diff: {:?}", level);
-            break;
-        } 
-    }
-    return diffs_valid
-}
+mod day_2;
 
 fn main() {
-    //println!("Day 1, Task 1 {}.", day_1::first());
-    //println!("Day 1, Task 2 {}.", day_1::second());
-
-    let file_str: String = String::from(include_str!("inputs/day2_input"));
-
-    let lines: Vec<String> = file_str.split("\n").map(|x| String::from(x)).collect();
-    
-    let res:  Vec<Vec<i32>> = lines.into_iter().map(|line| line.split_whitespace().map(|num| num.parse::<i32>().unwrap()).collect()).collect();
-
-    let safe_levels = res.into_iter().filter(|level| is_level_valid(&level)).count();
-
-    println!("{}", safe_levels);
-
-    return ();
+    println!("--------------------------------");
+    println!("Day 1, Task 1: {}.", day_1::first());
+    println!("Day 1, Task 2: {}.", day_1::second());
+    println!("--------------------------------");
+    println!("Day 2, Task 1: {}.", day_2::first());
 }
